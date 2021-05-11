0f2b0afdd34033f56b69fdad143866779c85c246
SakaiTaka23 <tt3.sakai@gmail.com>
2021-05-11 15:59:38 +0900

    既にログインだった場合に弾く仕組みを作った

--- frontend/pages/signin.tsx
+++ frontend/pages/signin.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import AuthLinks from '../components/organisms/AuthLinks';
+import useNotLogin from '../hooks/firebase/useNotLogin';
 
 const signin = () => {
+  useNotLogin();
   return <AuthLinks />;
 };
 
