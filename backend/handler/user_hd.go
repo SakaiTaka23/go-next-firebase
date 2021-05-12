87c443b0a874a76a9534867c7a459785b52d6c6e
SakaiTaka23 <tt3.sakai@gmail.com>
2021-05-12 18:41:57 +0900

    handlerの雛形作成

---
+++ backend/handler/user_hd.go
@@ -0,0 +1,16 @@
+package handler
+
+import "backend/usecase"
+
+type UserHandler struct {
+	userUsecase usecase.UserUsecase
+}
+
+func NewUserHandler(userUsecase usecase.UserUsecase) UserHandler {
+	userHandler := UserHandler{userUsecase: userUsecase}
+	return userHandler
+}
+
+func CheckLoginUser() {
+
+}
