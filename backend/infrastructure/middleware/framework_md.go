dea1bce73d289bf2bee85009b051e82ce702e8f1
SakaiTaka23 <tt3.sakai@gmail.com>
2021-05-14 17:40:26 +0900

    ログ出力をするようにした

--- backend/infrastructure/middleware/framework_md.go
+++ backend/infrastructure/middleware/framework_md.go
@@ -1,11 +1,21 @@
+package middleware
+
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
 func SetFrameworkMiddleware(app *fiber.App) *fiber.App {
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "http://127.0.0.1:3000",
 		AllowMethods: "GET",
 		AllowHeaders: "Authorization",
+	}))
+
+	app.Use(logger.New(logger.Config{
+		Format:   "[${time}] ${method} ${path} - ${status}\n",
+		TimeZone: "Asia/Tokyo",
 	}))
 
 	return app
