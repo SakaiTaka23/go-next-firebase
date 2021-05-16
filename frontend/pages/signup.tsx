5fe9550f50ca69e80e937893c80d74e1e4351089
SakaiTaka23 <tt3.sakai@gmail.com>
2021-05-16 16:11:54 +0900

    signupに反映させた

--- frontend/pages/signup.tsx
+++ frontend/pages/signup.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import AuthLinks from '../components/organisms/AuthLinks';
+import MailPasswordForm from '../components/organisms/MailPasswordForm';
 import useNotLogin from '../hooks/firebase/useNotLogin';
 
 const signup = () => {
   useNotLogin();
-  return <AuthLinks />;
+  return (
+    <>
+      <MailPasswordForm />
+      <AuthLinks />
+    </>
+  );
 };
 
 export default signup;
