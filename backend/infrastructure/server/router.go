4d2cb630b544d6d88990450d583fc241930e575f
SakaiTaka23 <tt3.sakai@gmail.com>
2021-07-31 16:35:09 +0900

    認証デバッグ用ミドルウェア作成

--- backend/infrastructure/server/router.go
+++ backend/infrastructure/server/router.go
@@ -9,6 +9,7 @@ import (
 
 func SetRouter(app *fiber.App) *fiber.App {
 	middleware.SetAppMiddleware(app)
+	authMiddleware := middleware.SetAuthMiddleware()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
@@ -24,7 +25,7 @@ func SetRouter(app *fiber.App) *fiber.App {
 
 	userHandler := injector.InjectUserHandler()
 
-	user := app.Group("/user", middleware.AuthMiddleware)
+	user := app.Group("/user", authMiddleware)
 	user.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
 			"message": "Private",
