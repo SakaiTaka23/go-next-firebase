72e53eac54e898a15e001974266acfc17009121f
SakaiTaka23 <tt3.sakai@gmail.com>
2021-07-31 15:25:20 +0900

    validation追加

--- backend/handler/request/user_req.go
+++ backend/handler/request/user_req.go
@@ -1,5 +1,15 @@
 package request
 
+import "github.com/go-playground/validator/v10"
+
 type CreateUser struct {
-	UserName string `json:"name"`
+	Name string `json:"name" validate:"required"`
+}
+
+func (c *CreateUser) Validate() error {
+	validate := validator.New()
+	if err := validate.Struct(c); err != nil {
+		return err
+	}
+	return nil
 }
