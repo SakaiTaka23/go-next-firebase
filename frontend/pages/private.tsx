1f7ef0b6ea08b75cd7ccc3e3e77ee96ed90a3824
SakaiTaka23 <tt3.sakai@gmail.com>
2021-08-01 17:44:42 +0900

    ログインしていない場合の処理追加

--- frontend/pages/private.tsx
+++ frontend/pages/private.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../hooks/firebase/authContext';
 
 const privateFC = () => {
+  const { user } = useContext(AuthContext);
+  if (!user) {
+    return <div>you are not logged in</div>;
+  }
   return <div>private page</div>;
 };
 
