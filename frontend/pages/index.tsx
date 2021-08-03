e6f181b528369ddc5b239f017f058a9f43599e76
SakaiTaka23 <tt3.sakai@gmail.com>
2021-08-03 11:14:44 +0900

    試しに叩いてみた

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react';
+import useSWR from 'swr';
+import axiosInstance from '../hooks/api/axios';
 import { AuthContext } from '../hooks/firebase/authContext';
 import { useFirebase } from '../hooks/firebase/useFirebase';
 
@@ -6,10 +8,15 @@ const Index = () => {
   const { user } = useContext(AuthContext);
   const { Logout } = useFirebase();
 
+  const fetcher = (url: string) => axiosInstance.get(url).then((res) => res.data);
+  const { data } = useSWR('/', fetcher);
+
   return (
     <div>
       <pre>{user ? user.displayName + 'でログインしています' : 'ログインしていません'}</pre>
       <button onClick={() => Logout()}>Logout</button>
+
+      <h1>{data ? data.message : 'no message'}</h1>
     </div>
   );
 };
