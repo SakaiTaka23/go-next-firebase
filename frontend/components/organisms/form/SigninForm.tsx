96a2d5e09ce7a4c120bc1fd3f601c4617cbd93c1
SakaiTaka23 <tt3.sakai@gmail.com>
2021-10-22 15:43:06 +0900

    各種form更新

--- frontend/components/organisms/form/SigninForm.tsx
+++ frontend/components/organisms/form/SigninForm.tsx
@@ -1,31 +1,11 @@
 import React from 'react';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import { Avatar, Container, makeStyles, Typography } from '@material-ui/core';
 import { FormProvider, useForm } from 'react-hook-form';
+import { Avatar, Box, Container, CssBaseline, Typography } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import EmailInput from '../input/EmailInput';
 import PasswordInput from '../input/PassowrdInput';
 import SubmitButton from '../../molecules/SubmitButton';
 import { useFirebase } from '../../../hooks/firebase/useFirebase';
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
 
 type MailPasswordRequest = {
   email: string;
@@ -33,7 +13,6 @@ type MailPasswordRequest = {
 };
 
 const SignInForm = () => {
-  const classes = useStyles();
   const methods = useForm();
   const { SignIn } = useFirebase();
 
@@ -42,22 +21,30 @@ const SignInForm = () => {
   };
 
   return (
-    <Container maxWidth='xs'>
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component='h1' variant='h5'>
           Sign In
         </Typography>
         <FormProvider {...methods}>
-          <form className={classes.form} onSubmit={methods.handleSubmit(submit)}>
+          <Box component='form' onSubmit={methods.handleSubmit(submit)} sx={{ mt: 1 }}>
             <EmailInput />
             <PasswordInput />
             <SubmitButton />
-          </form>
+          </Box>
         </FormProvider>
-      </div>
+      </Box>
     </Container>
   );
 };
--- frontend/components/organisms/form/SignupForm.tsx
+++ frontend/components/organisms/form/SignupForm.tsx
@@ -1,32 +1,12 @@
 import React from 'react';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import { Avatar, Container, makeStyles, Typography } from '@material-ui/core';
 import { FormProvider, useForm } from 'react-hook-form';
+import { Avatar, Box, Container, CssBaseline, Typography } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { useFirebase } from '../../../hooks/firebase/useFirebase';
 import EmailInput from '../input/EmailInput';
 import PasswordInput from '../input/PassowrdInput';
 import SubmitButton from '../../molecules/SubmitButton';
 import NameInput from '../input/NameInput';
-import { useFirebase } from '../../../hooks/firebase/useFirebase';
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
 
 type MailPasswordRequest = {
   name: string;
@@ -35,7 +15,6 @@ type MailPasswordRequest = {
 };
 
 const SignUpForm = () => {
-  const classes = useStyles();
   const methods = useForm();
   const { SignUp } = useFirebase();
 
@@ -44,23 +23,31 @@ const SignUpForm = () => {
   };
 
   return (
-    <Container maxWidth='xs'>
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component='h1' variant='h5'>
           Sign Up
         </Typography>
         <FormProvider {...methods}>
-          <form className={classes.form} onSubmit={methods.handleSubmit(submit)}>
+          <Box component='form' onSubmit={methods.handleSubmit(submit)} sx={{ mt: 3 }}>
             <NameInput />
             <EmailInput />
             <PasswordInput />
             <SubmitButton />
-          </form>
+          </Box>
         </FormProvider>
-      </div>
+      </Box>
     </Container>
   );
 };
