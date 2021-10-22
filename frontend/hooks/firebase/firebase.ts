a4a098e34beb31a62c092aafe27d1f4d5d8b5b1a
SakaiTaka23 <tt3.sakai@gmail.com>
2021-10-22 13:43:07 +0900

    初期化を効率化

--- frontend/hooks/firebase/firebase.ts
+++ frontend/hooks/firebase/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { getApp, getApps, initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -9,6 +9,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = getApps().length < 1 ? initializeApp(firebaseConfig) : getApp();
 
 export default firebaseApp;
