e6ec791b3a20a7c33c233ff5d1a71f7666146e61
SakaiTaka23 <tt3.sakai@gmail.com>
2021-10-22 12:19:37 +0900

    初期化を更新

--- frontend/hooks/firebase/firebase.ts
+++ frontend/hooks/firebase/firebase.ts
@@ -1,5 +1,4 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -10,6 +9,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+const firebaseApp = initializeApp(firebaseConfig);
 
-export default firebase;
+export default firebaseApp;
